7f6606a1f9c9d5823fa018b5b9a9ab7912554a0d
Patrick Del Conte <patrick.delconte@umb.ch>
2018-09-20 22:42:07 +0200

    add size, isEmpty(), getOrElse()

--- src/main/kotlin/screeps/api/types.kt
+++ src/main/kotlin/screeps/api/types.kt
@@ -11,6 +11,7 @@ inline operator fun <F, S> JsPair<F, S>.component2(): S = asDynamic()[1].unsafeC
 inline fun <F, S> JsPair<F, S>.toPair() = Pair(this.component1(), this.component2())
 
 inline operator fun <K, V> Record<K, V>.get(key: K): V? = this.asDynamic()[key].unsafeCast<V?>()
+inline fun <K, V> Record<K, V>.getOrElse(key: K, defaultValue: () -> V): V = get(key) ?: defaultValue()
 inline operator fun <K, V> MutableRecord<K, V>.set(key: K, value: V) {
     this.asDynamic()[key] = value
 }
@@ -18,6 +19,9 @@ inline operator fun <K, V> MutableRecord<K, V>.set(key: K, value: V) {
 inline val <K, V>Record<K, V>.values: Array<V> get () = js("Object").values(this).unsafeCast<Array<V>>()
 inline val <K, V>Record<K, V>.keys: Array<K> get () = js("Object").keys(this).unsafeCast<Array<K>>()
 inline val <K, V>Record<K, V>.entries: Array<JsPair<K, V>> get() = js("Object").entries(this).unsafeCast<Array<JsPair<K, V>>>()
+inline val <K, V>Record<K, V>.size: Int get() = keys.size
+inline fun <K, V> Record<K, V>.isEmpty() = keys.isEmpty()
+inline fun <K, V> Record<K, V>.isNotEmpty() = keys.isNotEmpty()
 
 inline operator fun <K, V> Record<K, V>.iterator(): Iterator<JsPair<K, V>> = this.entries.iterator()
 inline fun <K, V> Record<K, V>.asIterable(): Iterable<JsPair<K, V>> = this.entries.asIterable()
