1adc3ded8f3bd418807c436cf6100bc89802798a
Patrick Del Conte <patrick.delconte@umb.ch>
2018-09-20 23:00:06 +0200

    add unit tests for Records

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,10 @@
 import com.jfrog.bintray.gradle.BintrayExtension
+import com.moowork.gradle.node.NodeExtension
+import com.moowork.gradle.node.npm.NpmInstallTask
+import com.moowork.gradle.node.npm.NpmSetupTask
+import com.moowork.gradle.node.npm.NpmTask
+import com.moowork.gradle.node.task.NodeTask
+import com.moowork.gradle.node.task.SetupTask
 import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
 
 
@@ -6,6 +12,7 @@ plugins {
     id("kotlin2js") version "1.2.70"
     `maven-publish`
     id("com.jfrog.bintray") version "1.8.1"
+    id("com.moowork.node") version "1.2.0"
 }
 repositories {
     jcenter()
@@ -14,6 +21,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-js"))
+    testCompile(kotlin("test-js"))
 }
 
 group = "ch.delconte.screeps-kotlin"
@@ -25,11 +33,43 @@ val sourcesJar by tasks.registering(Jar::class) {
 }
 
 tasks {
-    "compileKotlin2Js"(Kotlin2JsCompile::class) {
+
+    val compileKotlin2Js by named<Kotlin2JsCompile>("compileKotlin2Js") {
         kotlinOptions {
             sourceMap = true
             moduleKind = "umd"
         }
+    }
+
+    val compileTestKotlin2Js by named<Kotlin2JsCompile>("compileTestKotlin2Js") {
+        kotlinOptions {
+            sourceMap = true
+            moduleKind = "commonjs"
+        }
+    }
+
+    val installMocha by registering(NpmTask::class) {
+        setArgs(listOf("install", "mocha"))
+    }
+
+    val populateNodeModules by registering(Copy::class) {
+        from(compileKotlin2Js.outputFile)
+        configurations.testCompile.forEach {
+            from(zipTree(it.absolutePath).matching { include("*.js") })
+        }
+
+        into("${buildDir}/node_modules")
+    }
+
+    val runMocha by registering(NodeTask::class) {
+        dependsOn(installMocha, compileTestKotlin2Js, populateNodeModules)
+
+        setScript(File("node_modules/mocha/bin/mocha"))
+        setArgs(listOf(compileTestKotlin2Js.outputFile.absolutePath))
+    }
+
+    named<Test>("test") {
+        dependsOn(runMocha)
     }
 }
 
---
+++ src/test/kotlin/TestRecords.kt
@@ -0,0 +1,63 @@
+@file:Suppress("UNCHECKED_CAST_TO_EXTERNAL_INTERFACE")
+
+import screeps.api.*
+import screeps.utils.memory.memoryOrDefault
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class TestRecords {
+
+    @Test
+    fun testContains() {
+        val people = js(
+            """
+            {
+                "alf" : {},
+                "bob" : {},
+            }
+        """
+        ) as Record<String, CreepMemory>
+
+        assertTrue { "alf" in people }
+        assertTrue { "bob" in people }
+        assertEquals(2, people.size)
+    }
+
+    @Test
+    fun testIterator() {
+        val creeps = createCreeps()
+
+        var count = 0
+        for ((name, _) in creeps) {
+            assertTrue { name in creeps }
+            count++
+        }
+
+        assertEquals(creeps.size, count)
+    }
+
+    @Test
+    fun testValues() {
+        val creeps = createCreeps()
+
+        assertEquals(creeps.size, creeps.values.count())
+        assertEquals(creeps.size * 2, creeps.values.map { it.health }.sumBy { it * 2 })
+    }
+
+    private val CreepMemory.health by memoryOrDefault(1)
+
+    private fun createCreeps() = js(
+        """
+            {
+                "alf" : {
+                    "color" : "blue"
+                },
+                "bob" : {},
+                "charlie" : {},
+                "emil" : {}
+            }
+        """
+    ) as Record<String, CreepMemory>
+}
+
