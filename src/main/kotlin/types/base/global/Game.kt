fdb42d5431ff22732d5f5ce2b53f669f01c3cc10
Patrick Del Conte <patrick.delconte@umb.ch>
2018-09-20 12:17:06 +0200

    expose all properties of Game as proper kotlin collections

--- src/main/kotlin/types/base/global/Game.kt
+++ src/main/kotlin/types/base/global/Game.kt
@@ -2,40 +2,52 @@ package types.base.global
 
 import types.base.StringDict
 import types.base.prototypes.*
-import types.base.prototypes.structures.Structure
+import types.base.prototypes.structures.OwnedStructure
 import types.base.prototypes.structures.StructureSpawn
+import types.base.toMap
+import types.base.valuesInternal
+import types.extensions.lazyPerTick
 import kotlin.js.Json
 
 external object Game {
-    /**
-     * A hash containing all your construction sites with their id as hash keys.
-     */
-    val constructionSites: StringDict<ConstructionSite>
+
     /**
      * An object containing information about your CPU usage
      */
     val cpu: CPU
-    /**
-     * A hash containing all your creeps with creep names as hash keys.
-     */
-    val creeps: StringDict<Creep>
-    /**
-     * A hash containing all your flags with flag names as hash keys.
-     */
-    val flags: StringDict<Flag>
     val gcl: GlobalControlLevel
     val map: GameMap
     val market: Market
+
+    /**
+     * System game tick counter. It is automatically incremented on every tick. [Learn more][http://docs.screeps.com/game-loop.html]
+     */
+    val time: Int
+
+    /**
+     * A hash containing all your construction sites with their id as hash keys.
+     */
+    internal val constructionSites: StringDict<ConstructionSite>
+
+    /**
+     * A hash containing all your creeps with creep names as hash keys.
+     */
+    internal val creeps: StringDict<Creep>
+    /**
+     * A hash containing all your flags with flag names as hash keys.
+     */
+    internal val flags: StringDict<Flag>
+
     /**
      * An object with your global resources that are bound to the account, like subscription tokens.
      * Each object key is a resource constant, values are resources amounts.
      */
-    val resources: StringDict<Int>
+    internal val resources: StringDict<Int>
     /**
      * A hash containing all the rooms available to you with room names as hash keys.
      * A room is visible if you have a creep or an owned structure in it.
      */
-    val rooms: StringDict<Room>
+    internal val rooms: StringDict<Room>
     /**
      * An object describing the world shard where your script is currently being executed in.
      */
@@ -43,15 +55,11 @@ external object Game {
     /**
      * A hash containing all your spawns with spawn names as hash keys.
      */
-    val spawns: StringDict<StructureSpawn>
+    internal val spawns: StringDict<StructureSpawn>
     /**
      * A hash containing all your structures with structure id as hash keys.
      */
-    val structures: StringDict<Structure>
-    /**
-     * System game tick counter. It is automatically incremented on every tick. [Learn more][http://docs.screeps.com/game-loop.html]
-     */
-    val time: Int
+    internal val structures: StringDict<OwnedStructure>
 
     /**
      * Get an object with the specified unique ID. It may be a game object of any type.
@@ -67,6 +75,19 @@ external object Game {
     fun notify(message: String, groupInterval: Int = definedExternally)
 }
 
+val Game.creeps: Map<String, Creep> by lazyPerTick { creeps.toMap() }
+val Game.creepsArray: List<Creep> by lazyPerTick { creeps.valuesInternal() }
+val Game.flags: Map<String, Flag> by lazyPerTick { flags.toMap() }
+val Game.flagsArray: List<Flag> by lazyPerTick { flags.valuesInternal() }
+val Game.rooms: Map<String, Room> by lazyPerTick { rooms.toMap() }
+val Game.roomArray: List<Room> by lazyPerTick { rooms.valuesInternal() }
+val Game.constructionSites: Map<String, ConstructionSite> by lazyPerTick { constructionSites.toMap() }
+val Game.constructionSitesArray: List<ConstructionSite> by lazyPerTick { constructionSites.valuesInternal() }
+val Game.spawns: Map<String, StructureSpawn>  by lazyPerTick { spawns.toMap() }
+val Game.spawnsArray: List<StructureSpawn> by lazyPerTick { spawns.valuesInternal() }
+val Game.structures: Map<String, OwnedStructure> by lazyPerTick { structures.toMap() }
+val Game.structuresArray: List<OwnedStructure> by lazyPerTick { structures.valuesInternal() }
+val Game.resources: Map<String, Int> by lazyPerTick { resources.toMap() }
 
 private typealias ShardLimits = StringDict<Int>
 
--- src/main/kotlin/types/base/types.kt
+++ src/main/kotlin/types/base/types.kt
@@ -8,35 +8,33 @@ typealias StringDict<V> = JsDict<String, V>
 external interface JsDict<K : Any, V>
 
 @Suppress("NOTHING_TO_INLINE")
-inline operator fun <V> StringDict<V>.get(key: String): V? = asDynamic()[key] as? V
+internal inline operator fun <V> StringDict<V>.get(key: String): V? = asDynamic()[key] as? V
 
-inline operator fun <V> StringDict<V>.get(key: StringConstant): V = asDynamic()[key] as V
-inline operator fun <K : IntegerConstant, V> JsDict<K, V>.get(key: K): V = asDynamic()[key] as V
+internal inline operator fun <V> StringDict<V>.get(key: StringConstant): V = asDynamic()[key] as V
+internal inline operator fun <K : IntegerConstant, V> JsDict<K, V>.get(key: K): V = asDynamic()[key] as V
 
-val <V> StringDict<V>.keys: Array<String>
-    get() = js("Object").keys(this) as? Array<String> ?: emptyArray()
 
-class Entry<K, V>(override val key: K, override val value: V) : Map.Entry<K, V>
-
-@Suppress("NOTHING_TO_INLINE")
-inline operator fun <V> StringDict<V>.iterator(): Iterator<Map.Entry<String, V>> {
-    return object : Iterator<Map.Entry<String, V>> {
-        var currentIndex = 0
-
-        override fun hasNext(): Boolean = currentIndex < keys.size
-
-        override fun next(): Map.Entry<String, V> {
-            val key = keys[currentIndex]
-            currentIndex += 1
-            val value = this@iterator.asDynamic()[key] as V
-            return Entry(key, value)
-        }
+internal fun <V> StringDict<V>.valuesInternal(): List<V> {
+    // this is not an actual kotlin array, but it does provide an iterator
+    val values = unsafeValues()
+    val size: Int = values.size
+    val list = ArrayList<V>(size)
+    for (v in values) {
+        list.add(v)
     }
+    return list
 }
 
-fun <V> StringDict<V>.toMap(): Map<String, V> {
-    val map: MutableMap<String, V> = linkedMapOf()
-    for (key in keys) {
+// these are not real arrays in the kotlin sense, but arraylike enough
+internal fun <T> StringDict<T>.unsafeKeys(): Array<String> = js("Object").keys(this)
+
+internal fun <T> StringDict<T>.unsafeValues(): Array<T> = js("Object").keys(this)
+internal fun <T> StringDict<T>.unsafeEntries(): Array<T> = js("Object").keys(this)
+
+
+internal fun <V> StringDict<V>.toMap(): Map<String, V> {
+    val map: MutableMap<String, V> = HashMap(20)
+    for (key in unsafeKeys()) {
         val value: V? = this[key]
         if (value != null) {
             map[key] = value
@@ -48,11 +46,6 @@ fun <V> StringDict<V>.toMap(): Map<String, V> {
 }
 
 external interface MutableStringDict<V> : StringDict<V>
-
-@Suppress("NOTHING_TO_INLINE")
-inline operator fun <V> MutableStringDict<V>.set(key: String, value: V) {
-    asDynamic()[key] = value
-}
 
 class Filter(val filter: dynamic)
 
