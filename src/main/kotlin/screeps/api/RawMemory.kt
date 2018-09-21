0e4d5bf90dabb686c6a57555842c432b86ade246
Jonas Damtoft <jonasdamtoft@hotmail.com>
2018-09-21 19:58:34 +0200

    implement RawMemory

---
+++ src/main/kotlin/screeps/api/RawMemory.kt
@@ -0,0 +1,22 @@
+@file:Suppress("unused")
+
+package screeps.api
+
+external object RawMemory {
+    val segments: Record<Int, String>
+    val foreignSegment: ForeignSegment?
+    val interShardSegment: String?
+
+    fun get(): String
+    fun set(value: String)
+    fun setActiveSegments(ids: Array<Int>)
+    fun setActiveForeignSegment(username: String, id: Int? = definedExternally)
+    fun setDefaultPublicSegment(id: Int?)
+    fun setPublicSegments(ids: Array<Int>)
+
+    interface ForeignSegment {
+        val username: String
+        val id: Int
+        val data: String
+    }
+}
