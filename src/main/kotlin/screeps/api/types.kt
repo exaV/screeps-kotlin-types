57c44b359f0cb5557992a983d0ad86dccf0e7507
Jonas Damtoft <jonasdamtoft@hotmail.com>
2018-09-21 17:08:40 +0200

    adds extra extension functions to Record

--- src/main/kotlin/screeps/api/types.kt
+++ src/main/kotlin/screeps/api/types.kt
@@ -1,6 +1,36 @@
 package screeps.api
 
-import screeps.utils.memory.jsObject
+/**
+ * Instantiate an anonymous js object.
+ *
+ * Therefore this method should only be used with external interfaces to initialize memory
+ * ## Warning: You have to initialize all non-nullable properties!
+ * Otherwise you *will* get a NullpointerException on the first read
+ *
+ *
+ * ### Example
+ *
+ *     external interface Foo {
+ *         var name : String
+ *     }
+ *
+ *     var peter = jsObject {
+ *         name = "Peter"
+ *     }
+ *
+ *
+ * Is the same as the following js snippet
+ *
+ *     let peter = {
+ *         name = "Peter"
+ *     }
+ *
+ */
+fun <T> jsObject(block: T.() -> Unit): T = Any().unsafeCast<T>().also(block)
+
+class Filter(val filter: dynamic)
+
+external fun delete(p: dynamic): Boolean = definedExternally
 
 external interface JsPair<out F, out S>
 external interface Record<in K, out V>
@@ -8,9 +38,10 @@ external interface MutableRecord<in K, V> : Record<K, V>
 
 inline operator fun <F, S> JsPair<F, S>.component1(): F = asDynamic()[0].unsafeCast<F>()
 inline operator fun <F, S> JsPair<F, S>.component2(): S = asDynamic()[1].unsafeCast<S>()
-inline fun <F, S> JsPair<F, S>.toPair() = Pair(this.component1(), this.component2())
+inline fun <F, S> JsPair<F, S>.toPair() = Pair(component1(), component2())
 
-inline operator fun <K, V> Record<K, V>.get(key: K): V? = this.asDynamic()[key].unsafeCast<V?>()
+inline operator fun <K, V> Record<K, V>.get(key: K): V? = asDynamic()[key].unsafeCast<V?>()
+inline fun <K, V> Record<K, V>.getOrDefault(key: K, defaultValue: V): V = get(key) ?: defaultValue
 inline fun <K, V> Record<K, V>.getOrElse(key: K, defaultValue: () -> V): V = get(key) ?: defaultValue()
 inline operator fun <K, V> MutableRecord<K, V>.set(key: K, value: V) {
     this.asDynamic()[key] = value
@@ -20,17 +51,20 @@ inline val <K, V>Record<K, V>.values: Array<V> get () = js("Object").values(this
 inline val <K, V>Record<K, V>.keys: Array<K> get () = js("Object").keys(this).unsafeCast<Array<K>>()
 inline val <K, V>Record<K, V>.entries: Array<JsPair<K, V>> get() = js("Object").entries(this).unsafeCast<Array<JsPair<K, V>>>()
 inline val <K, V>Record<K, V>.size: Int get() = keys.size
+
 inline fun <K, V> Record<K, V>.isEmpty() = keys.isEmpty()
 inline fun <K, V> Record<K, V>.isNotEmpty() = keys.isNotEmpty()
+inline operator fun <K, V> Record<K, V>.contains(key: K): Boolean = containsKey(key)
+inline fun <K, V> Record<K, V>.containsKey(key: K): Boolean = keys.contains(key)
+inline fun <K, V> Record<K, V>.containsValue(value: V): Boolean = values.contains(value)
 
-inline operator fun <K, V> Record<K, V>.iterator(): Iterator<JsPair<K, V>> = this.entries.iterator()
-inline fun <K, V> Record<K, V>.asIterable(): Iterable<JsPair<K, V>> = this.entries.asIterable()
+inline operator fun <K, V> Record<K, V>.iterator(): Iterator<JsPair<K, V>> = entries.iterator()
+inline fun <K, V> Record<K, V>.asIterable(): Iterable<JsPair<K, V>> = entries.asIterable()
+inline fun <K, V> Record<K, V>.asSequence(): Sequence<JsPair<K, V>> = entries.asSequence()
 
-inline operator fun <K, V> Record<K, V>.contains(value: K): Boolean = this.keys.contains(value)
-
-fun <K, V> Record<K, V>.toMap(): Map<K, V> = this.unsafeCast<MutableRecord<K, V>>().toMap()
+fun <K, V> Record<K, V>.toMap(): Map<K, V> = unsafeCast<MutableRecord<K, V>>().toMap()
 fun <K, V> MutableRecord<K, V>.toMap(): MutableMap<K, V> = mutableMapOf<K, V>().also { map ->
-    this.entries.forEach { (k, v) ->
+    entries.forEach { (k, v) ->
         map[k] = v
     }
 }
@@ -41,13 +75,9 @@ fun <K, V> recordOf(vararg pairs: Pair<K, V>): Record<K, V> = mutableRecordOf(*p
 fun <K, V> mutableRecordOf(vararg pairs: Pair<K, V>): MutableRecord<K, V> =
     jsObject {
         pairs.forEach { (k, v) ->
-            this[k] = v
+            set(k, v)
         }
     }
-
-class Filter(val filter: dynamic)
-
-external fun delete(p: dynamic): Boolean = definedExternally
 
 sealed class Result<out E, out V> {
     open val error: E? = null
--- src/main/kotlin/screeps/utils/memory/CreepMemory.kt
+++ src/main/kotlin/screeps/utils/memory/CreepMemory.kt
@@ -43,32 +43,3 @@ fun <T> memory(): ReadWriteProperty<CreepMemory, T?> = CreepMemoryDelegate(null)
  */
 fun <T : Any> memoryOrDefault(default: T): ReadWriteProperty<CreepMemory, T> = CreepMemoryDelegate(default)
 
-/**
- * Instantiate an anonymous js object.
- *
- * Therefore this method should only be used with external interfaces to initialize memory
- * ## Warning: You have to initialize all non-nullable properties!
- * Otherwise you *will* get a NullpointerException on the first read
- *
- *
- * ### Example
- *
- *     external interface Foo {
- *         var name : String
- *     }
- *
- *     var peter = jsObject {
- *         name = "Peter"
- *     }
- *
- *
- * Is the same as the following js snippet
- *
- *     let peter = {
- *         name = "Peter"
- *     }
- *
- */
-fun <T> jsObject(block: T.() -> Unit): T = Any().unsafeCast<T>().also(block)
-
-
