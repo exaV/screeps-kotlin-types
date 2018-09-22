bf337e231f8c27eff1b2ba33f5742c5dc6948f83
Patrick Del Conte <patrick.delconte@umb.ch>
2018-09-22 12:14:31 +0200

    move Record utils into its own file

--- src/main/kotlin/screeps/utils/Interop.kt
+++ src/main/kotlin/screeps/utils/Interop.kt
@@ -1,7 +1,5 @@
 package screeps.utils
 
-import screeps.api.*
-
 /**
  * Instantiate an anonymous js object.
  *
@@ -30,42 +28,7 @@ import screeps.api.*
  */
 fun <T> jsObject(block: T.() -> Unit): T = Any().unsafeCast<T>().also(block)
 
-class Filter(val filter: dynamic)
-
 external fun delete(p: dynamic): Boolean = definedExternally
-
-// Creators
-
-fun <K, V> recordOf(vararg pairs: Pair<K, V>): Record<K, V> = mutableRecordOf(*pairs)
-fun <K, V> mutableRecordOf(vararg pairs: Pair<K, V>): MutableRecord<K, V> =
-    jsObject {
-        pairs.forEach { (k, v) ->
-            set(k, v)
-        }
-    }
-
-// Functions
-
-inline operator fun <K, V> Record<K, V>.contains(key: K): Boolean = containsKey(key)
-inline fun <K, V> Record<K, V>.containsKey(key: K): Boolean = keys.contains(key)
-inline fun <K, V> Record<K, V>.containsValue(value: V): Boolean = values.contains(value)
-
-inline fun <K, V> Record<K, V>.getOrDefault(key: K, defaultValue: V): V = get(key) ?: defaultValue
-inline fun <K, V> Record<K, V>.getOrElse(key: K, defaultValue: () -> V): V = get(key) ?: defaultValue()
-inline fun <K, V> Record<K, V>.isEmpty() = keys.isEmpty()
-inline fun <K, V> Record<K, V>.isNotEmpty() = keys.isNotEmpty()
-
-// Converters
-
-inline fun <K, V> Record<K, V>.asIterable(): Iterable<JsPair<K, V>> = entries.asIterable()
-inline fun <K, V> Record<K, V>.asSequence(): Sequence<JsPair<K, V>> = entries.asSequence()
-inline fun <F, S> JsPair<F, S>.toPair() = Pair(component1(), component2())
-inline fun <K, V> Record<K, V>.toMap(): Map<K, V> = unsafeCast<MutableRecord<K, V>>().toMap()
-fun <K, V> MutableRecord<K, V>.toMap(): MutableMap<K, V> = mutableMapOf<K, V>().also { map ->
-    entries.forEach { (k, v) ->
-        map[k] = v
-    }
-}
 
 sealed class Result<out E, out V> {
     open val error: E? = null
