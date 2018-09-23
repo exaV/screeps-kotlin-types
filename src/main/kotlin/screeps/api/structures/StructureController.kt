5e6cefb4900edf3df9efbddbebfccddd8b402106
Jonas Damtoft <jonasdamtoft@hotmail.com>
2018-09-23 14:31:52 +0200

    move StructureController return type interfaces to inner

--- src/main/kotlin/screeps/api/structures/StructureController.kt
+++ src/main/kotlin/screeps/api/structures/StructureController.kt
@@ -8,25 +8,25 @@ abstract external class StructureController : Structure, Owned {
     val level: Int
     val progress: Int
     val progressTotal: Int
-    val reservation: ReservationDefinition?
+    val reservation: Reservation?
     val safeMode: Int?
     val safeModeAvailable: Int
     val safeModeCooldown: Int?
-    val sign: SignDefinition?
+    val sign: Sign?
     val ticksToDowngrade: Int
     val upgradeBlocked: Int
     fun activateSafeMode(): ScreepsReturnCode
     fun unclaim(): ScreepsReturnCode
-}
 
-external interface ReservationDefinition {
-    var username: String
-    var ticksToEnd: Int
-}
+    interface Reservation {
+        var username: String
+        var ticksToEnd: Int
+    }
 
-external interface SignDefinition {
-    var username: String
-    var text: String
-    var time: Int
-    var datetime: Date
+    interface Sign {
+        var username: String
+        var text: String
+        var time: Int
+        var datetime: Date
+    }
 }
