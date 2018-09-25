03452e4fa1e3bade692f8cd82e2feefb1158d29c
Patrick Del Conte <patrick.delconte@umb.ch>
2018-09-25 22:36:21 +0200

    verify JSON.parse(JSON.stringify()) works for each MemoryDelegate

--- src/test/kotlin/memory/TestDelegatesWithDefault.kt
+++ src/test/kotlin/memory/TestDelegatesWithDefault.kt
@@ -1,4 +1,6 @@
 import screeps.api.CreepMemory
+import screeps.api.SearchOptions
+import screeps.utils.jsObject
 import screeps.utils.memory.memoryOrDefault
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -6,13 +8,16 @@ import kotlin.test.assertEquals
 
 class TestDelegatesWithDefault {
 
-    var CreepMemory.priority by memoryOrDefault(0)
+    var CreepMemory.priority: Int by memoryOrDefault(0)
+    var CreepMemory.options: SearchOptions by memoryOrDefault(jsObject<SearchOptions> {
+        maxCost = 10
+    })
 
-    enum class Color {
+    private enum class Color {
         RED, GREEN
     }
 
-    var CreepMemory.color by memoryOrDefault(Color.GREEN)
+    private var CreepMemory.color by memoryOrDefault(Color.GREEN)
 
     @Test
     fun testDefaultValue() {
@@ -25,18 +30,33 @@ class TestDelegatesWithDefault {
     @Test
     fun testEnum() {
         val memory = js("{}").unsafeCast<CreepMemory>()
-
-
         memory.color = Color.RED
-
         assertEquals(Color.RED, memory.color)
+
+        val parsed = JSON.parse<CreepMemory>(JSON.stringify(memory))
+        assertEquals(Color.RED, parsed.color)
+
     }
 
     @Test
     fun testWrite() {
         val memory = js("{}").unsafeCast<CreepMemory>()
         memory.priority = 15
-
         assertEquals(15, memory.priority)
+
+        val parsed = JSON.parse<CreepMemory>(JSON.stringify(memory))
+        assertEquals(15, parsed.priority)
+    }
+
+    @Test
+    fun modifyDefaultObject() {
+        val memory = js("{}").unsafeCast<CreepMemory>()
+        memory.options.flee = true
+        assertEquals(true, memory.options.flee, "modifications on default object should work")
+        assertEquals(10, memory.options.maxCost)
+
+        val parsed = JSON.parse<CreepMemory>(JSON.stringify(memory))
+        assertEquals(true, parsed.options.flee, "modification should still be present after deserialisation")
+        assertEquals(10, parsed.options.maxCost)
     }
 }
--- src/test/kotlin/memory/TestNullableDelegates.kt
+++ src/test/kotlin/memory/TestNullableDelegates.kt
@@ -10,14 +10,12 @@ import kotlin.test.*
 
 class TestNullableDelegates {
 
-    var CreepMemory.priority by memory<Int>()
-    var CreepMemory.message by memory<String>()
-    var CreepMemory.options by memory<SearchOptions>()
+    var CreepMemory.priority: Int? by memory()
+    var CreepMemory.message: String? by memory()
+    var CreepMemory.options: SearchOptions? by memory()
 
     @Test
-    fun testNullableMemory() {
-
-
+    fun existingValue() {
         val memory = js(
             """
             {
@@ -26,22 +24,25 @@ class TestNullableDelegates {
         """
         ).unsafeCast<CreepMemory>()
 
-
         assertEquals(13, memory.priority)
-        assertNull(memory.message)
+        assertEquals(13, memory.priority) // must not be overwritten on access
+    }
 
+    @Test
+    fun testNullableMemory() {
+        val memory = js("{}").unsafeCast<CreepMemory>()
+
+        assertNull(memory.message)
+        // property is created on first access
         assertTrue { memory.unsafeCast<Record<String, String>>().contains("message") }
 
         memory.message = "Hello"
-
         assertEquals("Hello", memory.message)
         assertEquals("Hello", memory.unsafeCast<Record<String, String>>()["message"])
 
         val parsed = JSON.parse<CreepMemory>(JSON.stringify(memory))
-
         assertEquals("Hello", parsed.message)
         assertEquals("Hello", parsed.unsafeCast<Record<String, String>>()["message"])
-
     }
 
     @Test
@@ -53,12 +54,10 @@ class TestNullableDelegates {
         memory.options = jsObject<SearchOptions> {
             maxCost = 15
         }
-
         assertEquals(15, memory.options?.maxCost)
 
         val parsed = JSON.parse<CreepMemory>(JSON.stringify(memory))
-
-        assertNotNull(memory.options)
+        assertNotNull(parsed.options)
         assertEquals(15, parsed.options?.maxCost)
     }
 
