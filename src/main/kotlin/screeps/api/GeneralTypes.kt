436831fb8e9f9cad4614ae0ba381ee3056c021a2
Patrick Del Conte <patrick.delconte@umb.ch>
2018-10-01 14:16:01 +0200

    document Options

--- src/main/kotlin/screeps/api/GeneralTypes.kt
+++ src/main/kotlin/screeps/api/GeneralTypes.kt
@@ -11,6 +11,12 @@ external interface FilterOption<T> {
     var filter: ((T) -> Boolean)?
 }
 
+/**
+ * Indicates type is an external interface with only *mutable and nullable* properties.
+ * Thus it can be safely instantiated by [screeps.utils.jsObject]
+ *
+ * We provide the function [options] to create such objects
+ */
 external interface Options
 
 fun <T : Options> options(init: T.() -> Unit): T = jsObject(init).unsafeCast<T>()
