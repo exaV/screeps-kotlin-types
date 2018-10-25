44deec07ec18389d606f804f660a099cf073ae7b
Patrick Del Conte <p.del.conte@hotmail.ch>
2018-10-25 20:55:04 +0200

    Memory implements MemoryMarker so we can use memory-delegates on it

--- src/main/kotlin/screeps/api/Memory.kt
+++ src/main/kotlin/screeps/api/Memory.kt
@@ -1,17 +1,22 @@
 package screeps.api
 
-external object Memory : MutableRecord<String, dynamic> {
-    var creeps: MutableRecord<String, CreepMemory>
-    var flags: MutableRecord<String, FlagMemory>?
-    var rooms: MutableRecord<String, RoomMemory>
-    var spawns: MutableRecord<String, SpawnMemory>?
-
-}
-
 /**
  * Indicates that this type is always a child of the global _Memory_ object
  */
 external interface MemoryMarker
+
+/**
+ * A global plain object which can contain arbitrary data. You can access it both using the API and the Memory UI in the game editor.
+ *
+ * All extension properties on Memory should inherith the interface MemoryMarker
+ * @see MemoryMarker
+ */
+external object Memory : MutableRecord<String, dynamic>, MemoryMarker {
+    var creeps: MutableRecord<String, CreepMemory>
+    var flags: MutableRecord<String, FlagMemory>?
+    var rooms: MutableRecord<String, RoomMemory>
+    var spawns: MutableRecord<String, SpawnMemory>?
+}
 
 external interface CreepMemory : MemoryMarker
 external interface FlagMemory : MemoryMarker
