981ee341f49c74f36befd4f61243af9c97e91a42
Patrick Del Conte <p.del.conte@hotmail.ch>
2019-03-14 21:14:55 +0100

    update kotlin to 1.3.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,26 @@
 import com.jfrog.bintray.gradle.BintrayExtension
-import com.moowork.gradle.node.NodeExtension
-import com.moowork.gradle.node.npm.NpmInstallTask
-import com.moowork.gradle.node.npm.NpmSetupTask
 import com.moowork.gradle.node.npm.NpmTask
 import com.moowork.gradle.node.task.NodeTask
-import com.moowork.gradle.node.task.SetupTask
 import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
 
-
 plugins {
-    id("kotlin2js") version "1.3.0"
+    id("kotlin2js") version "1.3.21"
     `maven-publish`
     id("com.jfrog.bintray") version "1.8.1"
     id("com.moowork.node") version "1.2.0"
 }
+
 repositories {
     jcenter()
 }
 
-
 dependencies {
     implementation(kotlin("stdlib-js"))
-    testCompile(kotlin("test-js"))
+    testImplementation(kotlin("test-js"))
 }
 
 group = "ch.delconte.screeps-kotlin"
-version = "1.0.3"
+version = "1.0.4"
 
 val sourcesJar by tasks.registering(Jar::class) {
     classifier = "sources"
@@ -49,15 +44,11 @@ tasks {
     }
 
     val installMocha by registering(NpmTask::class) {
-        setArgs(listOf("install", "mocha"))
+        setArgs(listOf("install", "mocha", "kotlin", "kotlin-test"))
     }
 
     val populateNodeModules by registering(Copy::class) {
         from(compileKotlin2Js.outputFile)
-        configurations.testCompile.forEach {
-            from(zipTree(it.absolutePath).matching { include("*.js") })
-        }
-
         into("${buildDir}/node_modules")
     }
 
