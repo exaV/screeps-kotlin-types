4cccbf28be24d1063f9b37c670b71cf50bb65ab0
MaggNorway <MaggNorway@users.noreply.github.com>
2019-03-28 20:06:32 +0100

    Patch 3.3.0 (#24)
    * Add Global Power Level (GPL) in the same way as Global Control Level (GCL), and make the interface generic between them
    
    * Add new constants
    
    * Add PowerCreep class
    
    * Changes to RoomObject
    
    * Add isPowerEnabled to StructureController
    
    * Add InterShardMemory

--- src/main/kotlin/screeps/api/RoomObject.kt
+++ src/main/kotlin/screeps/api/RoomObject.kt
@@ -2,4 +2,11 @@ package screeps.api
 
 external interface RoomObject : HasPosition {
     val room: Room
+    val effects: List<RoomEffect>
+
+    interface RoomEffect {
+        val power: PowerEffectConstant
+        val level: Int
+        val ticksRemaining: Int
+    }
 }
