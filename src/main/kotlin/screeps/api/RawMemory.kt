789bba43b287d8bd93697f839d2b43d2ec1d47ef
Magnus Espeland <github@magg.no>
2019-04-14 14:47:12 +0200

    Add the RawMemory._parsed property (for the ._parsed black magic trick)

--- src/main/kotlin/screeps/api/RawMemory.kt
+++ src/main/kotlin/screeps/api/RawMemory.kt
@@ -4,6 +4,7 @@ external object RawMemory {
     val segments: MutableRecord<Int, String>
     val foreignSegment: ForeignSegment?
     val interShardSegment: String?
+    var _parsed: dynamic
 
     fun get(): String
     fun set(value: String)
