38080e45b518ce9b50b298c4937faa23ec892a61
Patrick Del Conte <patrick.delconte@umb.ch>
2019-06-21 09:14:54 +0200

    upgrade kotlin to 1.3.40 and use new js plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,9 @@
 import com.jfrog.bintray.gradle.BintrayExtension
-import com.moowork.gradle.node.npm.NpmTask
-import com.moowork.gradle.node.task.NodeTask
-import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
 
 plugins {
-    id("kotlin2js") version "1.3.30"
+    kotlin("js") version "1.3.40"
     `maven-publish`
     id("com.jfrog.bintray") version "1.8.1"
-    id("com.moowork.node") version "1.2.0"
 }
 
 repositories {
@@ -15,60 +11,31 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-js"))
     testImplementation(kotlin("test-js"))
 }
 
 group = "ch.delconte.screeps-kotlin"
-version = "1.3.1"
+version = "1.3.2"
 
-val sourcesJar by tasks.registering(Jar::class) {
-    classifier = "sources"
-    from(sourceSets["main"].allSource)
-}
-
-tasks {
-
-    val compileKotlin2Js by named<Kotlin2JsCompile>("compileKotlin2Js") {
-        kotlinOptions {
-            sourceMap = true
-            moduleKind = "commonjs"
-        }
+kotlin {
+    target {
+        useCommonJs()
+        nodejs()
     }
 
-    val compileTestKotlin2Js by named<Kotlin2JsCompile>("compileTestKotlin2Js") {
-        kotlinOptions {
-            sourceMap = true
-            moduleKind = "commonjs"
-        }
-    }
-
-    val installMocha by registering(NpmTask::class) {
-        setArgs(listOf("install", "mocha", "kotlin", "kotlin-test"))
-    }
-
-    val populateNodeModules by registering(Copy::class) {
-        from(compileKotlin2Js.outputFile)
-        into("${buildDir}/node_modules")
-    }
-
-    val runMocha by registering(NodeTask::class) {
-        dependsOn(installMocha, compileTestKotlin2Js, populateNodeModules)
-
-        setScript(File("node_modules/mocha/bin/mocha"))
-        setArgs(listOf(compileTestKotlin2Js.outputFile.absolutePath))
-    }
-
-    named<Test>("test") {
-        dependsOn(runMocha)
+    sourceSets["main"].dependencies {
+        implementation(kotlin("stdlib-js"))
     }
 }
+
+
+val kotlinSourcesJar by tasks
 
 publishing {
     publications {
-        register("mavenJava", MavenPublication::class) {
-            from(components["java"])
-            artifact(sourcesJar.get())
+        register("kotlin", MavenPublication::class) {
+            from(components["kotlin"])
+            artifact(kotlinSourcesJar)
         }
     }
 }
@@ -80,7 +47,7 @@ bintray {
     user = bintrayUser ?: ""
     key = bintrayApiKey ?: ""
     publish = true
-    setPublications("mavenJava")
+    setPublications("kotlin")
     pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
         repo = "screeps-kotlin"
         name = "screeps-kotlin-types"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,1 @@
-pluginManagement {
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "kotlin2js") {
-                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
-            }
-        }
-    }
-}
-
 rootProject.name = "screeps-kotlin-types"
