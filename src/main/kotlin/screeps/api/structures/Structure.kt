bb1928d37f16bfe21ba4be9a7ed58f130b0a44db
Patrick Del Conte <patrick.delconte@umb.ch>
2019-07-08 10:44:01 +0200

    remove body of class Structure as it is the same as IStructure

--- src/main/kotlin/screeps/api/structures/Structure.kt
+++ src/main/kotlin/screeps/api/structures/Structure.kt
@@ -1,14 +1,6 @@
 package screeps.api.structures
 
 import screeps.api.IStructure
-import screeps.api.ScreepsReturnCode
-import screeps.api.StructureConstant
 
 
-abstract external class Structure : IStructure {
-    override val structureType: StructureConstant
-
-    override fun destroy(): ScreepsReturnCode
-    override fun isActive(): Boolean
-    override fun notifyWhenAttacked(enabled: Boolean): ScreepsReturnCode
-}
+abstract external class Structure : IStructure
