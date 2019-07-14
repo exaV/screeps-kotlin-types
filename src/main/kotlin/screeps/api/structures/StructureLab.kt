40979cf844e3ee591c23ffb44f2d416765c65169
João Braz <joaofgbraz@gmail.com>
2019-07-14 22:12:52 +0100

    Fixed StructureLab#mineralType variable type to ResourceConstant.

--- src/main/kotlin/screeps/api/structures/StructureLab.kt
+++ src/main/kotlin/screeps/api/structures/StructureLab.kt
@@ -1,14 +1,11 @@
 package screeps.api.structures
 
-import screeps.api.Creep
-import screeps.api.EnergyContainer
-import screeps.api.Owned
-import screeps.api.ScreepsReturnCode
+import screeps.api.*
 
 abstract external class StructureLab : Structure, Owned, EnergyContainer {
     val cooldown: Int
     val mineralAmount: Int
-    val mineralType: String
+    val mineralType: ResourceConstant
     val mineralCapacity: Int
     fun boostCreep(creep: Creep, bodyPartsCount: Int = definedExternally): ScreepsReturnCode
     fun runReaction(lab1: StructureLab, lab2: StructureLab): ScreepsReturnCode
