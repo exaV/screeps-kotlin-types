a9913604a9505a0b7d77e8662f0e7837e3436010
Patrick Del Conte <p.del.conte@hotmail.ch>
2020-01-17 09:36:29 +0100

    Fixes 49

--- src/main/kotlin/screeps/api/structures/StructureTower.kt
+++ src/main/kotlin/screeps/api/structures/StructureTower.kt
@@ -3,7 +3,7 @@ package screeps.api.structures
 import screeps.api.*
 
 abstract external class StructureTower : Structure, Owned, StoreOwner {
-    fun attack(target: Creep): ScreepsReturnCode
-    fun heal(target: Creep): ScreepsReturnCode
+    fun attack(target: Attackable): ScreepsReturnCode
+    fun heal(target: GenericCreep): ScreepsReturnCode
     fun repair(target: IStructure): ScreepsReturnCode
 }
