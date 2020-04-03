35a6fa43dfcb83a2bec1cc940dac01a8a9d1323e
Patrick Del Conte <p.del.conte@hotmail.ch>
2020-04-03 21:47:16 +0200

    add an overload for treating null as 0 in numeric comparisons
    This is useful because it allows us to write
    `creep.store[RESOURCE_ENERGY] < creep.store.getCapacity()`
    which is possible in Javascript (but not plain Kotlin) because
    in Javascript `null` is treated as `0` in numeric comparisons
    
    Kotlin does not define such a function, likely because it does not want to assert
    how null should relate to other numbers (is it smaller than anything? larger? eq 0?)
    in pursuit of good interop with Screeps this library takes a stance

--- src/main/kotlin/screeps/api/Store.kt
+++ src/main/kotlin/screeps/api/Store.kt
@@ -22,6 +22,7 @@ external interface Store : Record<ResourceConstant, Int> {
      * Returns total used capacity for general purpose stores
      */
     fun getUsedCapacity(): Int
+
     /**
      * Returns the capacity used by the specified resource, or null in case of a not valid resource
      * for this store type
@@ -29,3 +30,7 @@ external interface Store : Record<ResourceConstant, Int> {
     fun getUsedCapacity(resource: ResourceConstant): Int?
 
 }
+
+// useful to compare entity.store[RESOURCE_ENERGY] < entity.store.getCapacity()
+// without this it turns into entity.store[RESOURCE_ENERGY] ?: 0 < entity.store.getCapacity() ?: 0
+operator fun Int?.compareTo(other: Int?): Int = compareValues(this ?: 0, other ?: 0)
