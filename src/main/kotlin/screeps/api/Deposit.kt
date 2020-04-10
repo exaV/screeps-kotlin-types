482964c709453405359f4bf6f68eda52f48b2310
Magnus Espeland <github@magg.no>
2020-04-10 20:04:14 +0200

    Add missing field and trait to Deposit

--- src/main/kotlin/screeps/api/Deposit.kt
+++ src/main/kotlin/screeps/api/Deposit.kt
@@ -1,5 +1,6 @@
 package screeps.api
 
-abstract external class Deposit : RoomObjectNotNull, Decaying, Harvestable, Identifiable {
+abstract external class Deposit : RoomObjectNotNull, Decaying, WithCooldown, Harvestable, Identifiable {
     val lastCooldown: Int
+    val depositType: DepositResourceConstant
 }
