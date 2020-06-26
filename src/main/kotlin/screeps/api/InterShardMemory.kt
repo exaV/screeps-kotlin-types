952586288ff40c8b228c03e0b3f85112e61f43b7
Magnus Espeland <github@magg.no>
2020-06-26 18:36:26 +0200

    Implement workaround for the potentially undefined object

--- src/main/kotlin/screeps/api/InterShardMemory.kt
+++ src/main/kotlin/screeps/api/InterShardMemory.kt
@@ -1,7 +1,19 @@
 package screeps.api
 
-external object InterShardMemory {
-    fun getLocal() : String
-    fun setLocal(value: String)
-    fun getRemote(shard: String) : String
+// The InterShardMemory is simply not defined on private servers, so we need to work around it
+
+class InterShardMemory {
+    companion object {
+        fun getLocal() : String
+                = eval("if(global.InterShardMemory) global.InterShardMemory.getLocal()")?.unsafeCast<String>()
+                ?: ""
+
+        fun setLocal(value: String)
+                = eval("if(global.InterShardMemory) global.InterShardMemory.setLocal($value)")
+
+        fun getRemote(shard: String) : String
+                = eval("if(global.InterShardMemory) global.InterShardMemory.getRemote($shard)")?.unsafeCast<String>()
+                ?: ""
+
+    }
 }
