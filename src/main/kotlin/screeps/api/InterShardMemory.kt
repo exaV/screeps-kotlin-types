63c75183a08596c6aa87bf9beed1229a2d1c1831
Magnus Espeland <github@magg.no>
2020-06-28 14:39:27 +0200

    Make InterShardMemory a nullable value on global instead

--- src/main/kotlin/screeps/api/InterShardMemory.kt
+++ src/main/kotlin/screeps/api/InterShardMemory.kt
@@ -1,17 +1,10 @@
 package screeps.api
 
-// The InterShardMemory is simply not defined on private servers, so we need to work around it
+// The InterShardMemory is simply not defined on private servers
+// Check if global.InterShardMemory is not null before using these
 
-class InterShardMemory {
-    companion object {
-        fun getLocal() : String?
-                = eval("if(global.InterShardMemory) global.InterShardMemory.getLocal()")?.unsafeCast<String>()
-
-        fun setLocal(value: String)
-                = eval("if(global.InterShardMemory) global.InterShardMemory.setLocal('$value')")
-
-        fun getRemote(shard: String) : String?
-                = eval("if(global.InterShardMemory) global.InterShardMemory.getRemote('$shard')")?.unsafeCast<String>()
-
-    }
+external interface InterShardMemory {
+    fun getLocal(): String?
+    fun setLocal(value: String)
+    fun getRemote(shard: String): String?
 }
