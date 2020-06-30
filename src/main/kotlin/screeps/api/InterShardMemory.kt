a7eea16363afec2417a5f7163c70734eeae2b6e7
Patrick Del Conte <p.del.conte@hotmail.ch>
2020-06-30 16:03:56 +0200

    Update InterShardMemory.kt

--- src/main/kotlin/screeps/api/InterShardMemory.kt
+++ src/main/kotlin/screeps/api/InterShardMemory.kt
@@ -1,8 +1,5 @@
 package screeps.api
 
-// The InterShardMemory is simply not defined on private servers
-// Check if global.InterShardMemory is not null before using these
-
 external interface InterShardMemory {
     fun getLocal(): String?
     fun setLocal(value: String)
