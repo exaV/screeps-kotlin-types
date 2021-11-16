77afdf3848acd9a074c9e0822961577ca38fdeda
cinling <1105412681@qq.com>
2021-11-16 14:32:51 +0800

    [lazyPerTick] support null value

--- src/main/kotlin/screeps/utils/Lazy.kt
+++ src/main/kotlin/screeps/utils/Lazy.kt
@@ -7,7 +7,7 @@ import kotlin.reflect.KProperty
 /**
  * Lazy property that computed at most once per tick
  */
-private class TickLazy<in P : Any?, T : Any>(val computeOncePerTick: P.() -> T) : ReadOnlyProperty<P, T> {
+private class TickLazy<in P : Any?, T : Any?>(val computeOncePerTick: P.() -> T) : ReadOnlyProperty<P, T> {
     var value: T? = null
     var tick: Int = -1
 
@@ -23,6 +23,6 @@ private class TickLazy<in P : Any?, T : Any>(val computeOncePerTick: P.() -> T) 
 /**
  * Creates a lazy property that computed at most once per tick
  */
-fun <P : Any?, T : Any> lazyPerTick(computeOncePerTick: P.() -> T): ReadOnlyProperty<P, T> {
+fun <P : Any?, T : Any?> lazyPerTick(computeOncePerTick: P.() -> T): ReadOnlyProperty<P, T> {
     return TickLazy(computeOncePerTick)
 }
