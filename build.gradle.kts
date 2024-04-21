a0c8c5987cd2ced0f7191cd4a7fce64e56035fee
exav <p.del.conte@hotmail.ch>
2024-04-21 19:44:39 +0200

    update publishing config for kotlin multiplatform

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,6 +78,6 @@ signing {
     val signingKey: String? by project
     val signingPassword: String? by project
     useInMemoryPgpKeys(signingKey, signingPassword)
-    sign(publishing.publications["kotlin"])
+    sign(publishing.publications)
 }
 
